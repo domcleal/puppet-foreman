ab098768f80ca37d1dc080c18ebb37c77bc26e26
Dominic Cleal <dcleal@redhat.com>
2013-05-29 10:55:03 +0100

    Workaround dbmigrate not knowing SCL, upstream fix 7908aba

--- manifests/database.pp
+++ manifests/database.pp
@@ -4,10 +4,16 @@ class foreman::database {
 
     class { $db_class: } ~>
     exec { "dbmigrate":
-      command     => "${foreman::app_root}/extras/dbmigrate",
+      command     => "/opt/rh/ruby193/root/usr/bin/rake db:migrate",
+      cwd         => $foreman::app_root,
       user        => $foreman::user,
-      environment => "HOME=${foreman::app_root}",
-      logoutput   => 'on_failure',
+      environment => [
+        "HOME=${foreman::app_root}",
+        "PATH=/opt/rh/ruby193/root/usr/bin",
+        "LD_LIBRARY_PATH=/opt/rh/ruby193/root/usr/lib64",
+        "RAILS_ENV=production"
+      ],
+      logoutput => 'on_failure',
       refreshonly => true,
     }
   }
